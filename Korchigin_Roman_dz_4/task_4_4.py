d7dc30d030d25c26dfee26a94378baa55667ff3e
Roman <korchigin5@gmail.com>
2022-02-11 20:16:35 +0300

    Korchigin_Roman_dz_4

---
+++ Korchigin_Roman_dz_4/task_4_4.py
@@ -0,0 +1,9 @@
+import utils
+
+print(utils.currency_rates("UZS"))
+print(utils.currency_rates("KRW"))
+print(utils.currency_rates("eur"))
+print(utils.currency_rates("gur"))
+
+# При запуске консоли и терминала, произошел ModuleNotFoundError: No module named 'utils'. Пытался переименовать
+# модуль. Результат такой же. Если есть возможность помогите разобраться в чем причина.
